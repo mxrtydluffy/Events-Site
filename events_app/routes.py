a038e782d6e01bd2f66eaaa05dae29c81346b7a5
mxrtydluffy <marty.marinda@students.dominican.edu>
2023-01-31 20:24:49 -0800

    attempted routes

--- events_app/routes.py
+++ events_app/routes.py
@@ -21,6 +21,8 @@ def index():
     # TODO: Get all events and send to the template
     
     return render_template('index.html')
+    events = Event.query.all()
+    return render_template('index.html', events=events)
 
 
 @main.route('/create', methods=['GET', 'POST'])
@@ -43,6 +45,14 @@ def create():
         # TODO: Create a new event with the given title, description, & 
         # datetime, then add and commit to the database
 
+        new_event = Event(
+            title = new_event_title,
+            description = new_event_description,
+            date_and_time = date_and_time
+        )
+        db.session.add(new_event)
+        db.session.commit()
+
         flash('Event created.')
         return redirect(url_for('main.index'))
     else:
@@ -55,13 +65,15 @@ def event_detail(event_id):
 
     # TODO: Get the event with the given id and send to the template
     
-    return render_template('event_detail.html')
+    event = Event.query.get(event_id)
+    return render_template('event_detail.html', event=event)
 
 
 @main.route('/event/<event_id>', methods=['POST'])
 def rsvp(event_id):
     """RSVP to an event."""
     # TODO: Get the event with the given id from the database
+    event = Event.query.get(event_id)
     is_returning_guest = request.form.get('returning')
     guest_name = request.form.get('guest_name')
 
@@ -70,16 +82,29 @@ def rsvp(event_id):
         # database, render the event_detail.html template, and pass in an error
         # message as `error`.
 
+        guest = Guest.query.filter_by(name=guest_name).first()
+        if guest is None:
+            error = "Error"
+            flash(error)
+
         # TODO: If the guest does exist, add the event to their 
         # events_attending, then commit to the database.
-        pass
+        
+        else:
+            guest.events_attending.append(event)
+            db.session.add(guest)
+            db.session.commit()
+
     else:
         guest_email = request.form.get('email')
         guest_phone = request.form.get('phone')
 
         # TODO: Create a new guest with the given name, email, and phone, and 
         # add the event to their events_attending, then commit to the database.
-        pass
+        new_guest = Guest(name=guest_name, email=guest_email, phone=guest_phone)
+        new_guest.events_attending.append(event)
+        db.session.add(new_guest)
+        db.session.commit()
     
     flash('You have successfully RSVP\'d! See you there!')
     return redirect(url_for('main.event_detail', event_id=event_id))
@@ -89,4 +114,5 @@ def rsvp(event_id):
 def guest_detail(guest_id):
     # TODO: Get the guest with the given id and send to the template
     
-    return render_template('guest_detail.html')
+    guest = Guest.query.get(id=guest_id).first()
+    return render_template('guest_detail.html', guest=guest) 
